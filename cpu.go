0432137d9c27f094254d9cc33947ea5a64776b15
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-05 20:59:43 +0300

    feat: implement GetCPUInfo function for retrieving detailed CPU information

--- cpu.go
+++ cpu.go
@@ -1,8 +1,130 @@
 package psutilgolangmohamedfadel
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CPUCoreFreq struct {
+	Core    string
+	MinFreq int
+	MaxFreq int
+}
 type CPUInfo struct {
-	NumOfCores string
+	NumOfCores int
 	ModelName  string
-	CacheSize  string
-	CPUMHz     string
+	CacheSize  int
+	CPUMHz     float64
+	Frequency  []CPUCoreFreq
+}
+
+func GetCPUInfo() (*CPUInfo, error) {
+	var cpuInfo CPUInfo
+
+	data, err := openAndReadFile("/proc/cpuinfo")
+	if err != nil {
+		return nil, err
+	}
+
+	cpuInfo.ModelName = getFieldValue(data, "model name")
+
+	for _, line := range data {
+		if strings.Contains(line, "processor") {
+			cpuInfo.NumOfCores++
+		}
+	}
+
+	var totalMHz float64
+	for _, line := range data {
+		if strings.Contains(line, "cpu MHz") {
+			if mhz, err := strconv.ParseFloat(strings.TrimSpace(strings.Split(line, ":")[1]), 64); err == nil {
+				totalMHz += mhz
+			}
+		}
+	}
+	cpuInfo.CPUMHz = totalMHz / float64(cpuInfo.NumOfCores)
+
+	d, err := os.ReadFile("/sys/devices/system/cpu/cpu0/cache/index0/size")
+	if err != nil {
+		return nil, err
+	}
+
+	value := strings.Split(string(d), "\n")[0]
+	value = value[:len(value)-1]
+	l1iCache, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, err
+	}
+
+	d2, err := os.ReadFile("/sys/devices/system/cpu/cpu0/cache/index1/size")
+	if err != nil {
+		return nil, err
+	}
+
+	value2 := strings.Split(string(d2), "\n")[0]
+	value2 = value2[:len(value2)-1]
+	l1dCache, err := strconv.Atoi(value2)
+	if err != nil {
+		return nil, err
+	}
+
+	d3, err := os.ReadFile("/sys/devices/system/cpu/cpu0/cache/index2/size")
+	if err != nil {
+		return nil, err
+	}
+
+	value3 := strings.Split(string(d3), "\n")[0]
+	value3 = value3[:len(value3)-1]
+	l2Cache, err := strconv.Atoi(value3)
+	if err != nil {
+		return nil, err
+	}
+
+	d4, err := os.ReadFile("/sys/devices/system/cpu/cpu0/cache/index3/size")
+	if err != nil {
+		return nil, err
+	}
+
+	value4 := strings.Split(string(d4), "\n")[0]
+	value4 = value4[:len(value4)-1]
+	l3Cache, err := strconv.Atoi(value4)
+	if err != nil {
+		return nil, err
+	}
+
+	cpuInfo.CacheSize = cpuInfo.NumOfCores*(l1iCache+l1dCache+l2Cache) + l3Cache
+
+	var coreNames []string
+	for i := range cpuInfo.NumOfCores {
+		coreName := []string{"cpu", strconv.Itoa(i)}
+		coreNames = append(coreNames, strings.Join(coreName, ""))
+	}
+
+	for i := range cpuInfo.NumOfCores {
+		var cpuCoreFreq CPUCoreFreq
+		cpuCoreFreq.Core = coreNames[i]
+
+		dataMin, err := os.ReadFile(fmt.Sprintf("/sys/devices/system/cpu/%s/cpufreq/cpuinfo_min_freq", coreNames[i]))
+		if err != nil {
+			return nil, err
+		}
+		if minFreq, err := strconv.Atoi(strings.Split(string(dataMin), "\n")[0]); err == nil {
+			cpuCoreFreq.MinFreq = minFreq
+		}
+
+		dataMax, err := os.ReadFile(fmt.Sprintf("/sys/devices/system/cpu/%s/cpufreq/cpuinfo_max_freq", coreNames[i]))
+		if err != nil {
+			return nil, err
+		}
+		if maxFreq, err := strconv.Atoi(strings.Split(string(dataMax), "\n")[0]); err == nil {
+			cpuCoreFreq.MaxFreq = maxFreq
+		}
+
+		cpuInfo.Frequency = append(cpuInfo.Frequency, cpuCoreFreq)
+
+	}
+
+	return &cpuInfo, nil
 }
