3dca4d6bf3107d68bc108b32aeaba167eb5b38f6
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-13 14:30:39 +0300

    feat: implement GetProcessInfo function for retrieving process information

--- proc.go
+++ proc.go
@@ -1,6 +1,49 @@
 package psutilgolangmohamedfadel
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 type ProcessInfo struct {
-	PID  string
-	Name string
+	PID   int
+	Name  string
+	State string
+}
+
+func GetProcessInfo() (*[]ProcessInfo, error) {
+	var procInfoList []ProcessInfo
+	var procInfo ProcessInfo
+	entries, err := os.ReadDir("/proc")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+		pid, err := strconv.Atoi(entry.Name())
+		if pid == 0 {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		lines, err := openAndReadFile(fmt.Sprintf("/proc/%d/status", pid))
+		if err != nil {
+			return nil, err
+		}
+		name := getFieldValue(lines, "Name")
+		state := getFieldValue(lines, "State")
+
+		procInfo.PID = pid
+		procInfo.Name = name
+		procInfo.State = state
+		procInfoList = append(procInfoList, procInfo)
+	}
+
+	return &procInfoList, nil
 }
